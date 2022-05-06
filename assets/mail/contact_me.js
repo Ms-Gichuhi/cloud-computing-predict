76651efc33a260edd4e322711709ab4bc31bce79
Ms-Gichuhi <67583513+Ms-Gichuhi@users.noreply.github.com>
2022-05-06 07:31:29 +0300

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "https://lhzp3j3iz2.execute-api.eu-west-1.amazonaws.com/default/predict-lambda",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
