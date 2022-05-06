c97d5cfc509389c94c07420d5e03bf751f640000
Ms-Gichuhi <67583513+Ms-Gichuhi@users.noreply.github.com>
2022-05-06 09:18:02 +0300

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "arn:aws:dynamodb:eu-west-1:901157483174:table/myportfolio-Dynamo-table",
+                url: "https://lhzp3j3iz2.execute-api.eu-west-1.amazonaws.com/default/predict-lambda",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
